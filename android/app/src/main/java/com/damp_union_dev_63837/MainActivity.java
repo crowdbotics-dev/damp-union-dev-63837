629311c205aba339a7cf2acf7b48771a36e076e0
Crowdbotics <engineering@crowdbotics.com>
2023-02-16 00:08:52 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/damp_union_dev_63837/MainActivity.java
@@ -0,0 +1,15 @@
+package com.damp_union_dev_63837;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "damp_union_dev_63837";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "damp-union-dev-63837",
+  url: "https://damp-union-dev-63837.botics.co",
+  api: "https://damp-union-dev-63837.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
